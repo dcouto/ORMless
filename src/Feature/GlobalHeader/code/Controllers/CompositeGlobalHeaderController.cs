a33a2d2be0bb34cdff6f72b05275f0f8076138ed
Douglas Couto <douglas.b.couto@gmail.com>
2018-10-25 09:20:05 -0400

    added Global Header.  added ORMlessBlankLayout.  renamed publishing target.

---
+++ src/Feature/GlobalHeader/code/Controllers/CompositeGlobalHeaderController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Feature.GlobalHeader.Controllers
+{
+    public class CompositeGlobalHeaderController : Controller
+    {
+        public ActionResult Default()
+        {
+            return View();
+        }
+    }
+}
